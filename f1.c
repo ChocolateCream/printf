f8b14e4b2a00ef1ede71f5dd3e323c64fa3f464f
Mina Garss <minazekry92@gmail.com>
2023-01-21 17:27:05 +0200

    fixing the n chars but messing the betty style

--- f1.c
+++ f1.c
@@ -25,12 +25,13 @@ int _putchar(char c)
 
 int _printf(const char *format, ...)
 {
-	int i, j;
+	int i, j, count;
 	char *s;
 	va_list args;
 
 	va_start(args, format);
 	i = 0;
+	count = 0;
 	if (format == NULL)
 		return (0);
 	while (format[i] != '\0')
@@ -38,6 +39,7 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			_putchar(format[i]);
+			count++;
 			i++;
 		}
 		else
@@ -47,19 +49,24 @@ int _printf(const char *format, ...)
 			{
 				case 'c':
 					_putchar(va_arg(args, int));
+					count++;
 					break;
 				case 's':
 					s = va_arg(args, char *);
 					for (j = 0; s[j]; j++)
+					{
 						_putchar(s[j]);
+						count++;
+					}
 					break;
 				case '%':
 					_putchar('%');
+					count++;
 					break;
 			}
 			i++;
 		}
 	}
 	va_end(args);
-	return (i - 1);
+	return (count);
 }
